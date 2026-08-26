32aa150b4f1faeb7e4939292390a1211716e8bdf
sayetoff <121031796+sayetoff@users.noreply.github.com>
2026-08-27 01:40:58 +0300

    Comment out API_ID and API_HASH error directive
    Comment out the error directive for missing API credentials.

--- Telegram/SourceFiles/config.h
+++ Telegram/SourceFiles/config.h
@@ -85,7 +85,7 @@ constexpr auto ApiHash = QT_STRINGIFY(TDESKTOP_API_HASH);
 // Your users will start getting internal server errors on login
 // if you deploy an app using those 'api_id' and 'api_hash'.
 
-#error You are required to provide API_ID and API_HASH.
+// #error You are required to provide API_ID and API_HASH.
 
 constexpr auto ApiId = 17349;
 constexpr auto ApiHash = "344583e45741c457fe1862106095a5eb";
